ffcde8ee9707af52d3da16254ac996371712b5e6
satyafromgiet <42096035+satyafromgiet@users.noreply.github.com>
2025-09-29 23:05:32 +0530

    Update player.js

--- player.js
+++ player.js
@@ -1,43 +1,55 @@
-// Elements
-const background = document.querySelector('#background');
-const thumbnail = document.querySelector('#thumbnail');
-const song = document.querySelector('#song');
-const songArtist = document.querySelector('.song-artist');
-const songTitle = document.querySelector('.song-title');
-const progressBar = document.querySelector('#progress-bar');
-const pPause = document.querySelector('#play-pause');
+const background = document.querySelector('#background'); // background derived from album cover below
+const thumbnail = document.querySelector('#thumbnail'); // album cover 
+const song = document.querySelector('#song'); // audio object
 
+const songArtist = document.querySelector('.song-artist'); // element where track artist appears
+const songTitle = document.querySelector('.song-title'); // element where track title appears
+const progressBar = document.querySelector('#progress-bar'); // element where progress bar appears
+let pPause = document.querySelector('#play-pause'); // element where play and pause image appears
+
+songIndex = 0;
+songs = ['./assets/music/beyonce.mp3', './assets/music/dontstartnow.mp3']; // object storing paths for audio objects
+thumbnails = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png']; // object storing paths for album covers and backgrounds
+songArtists = ['Beyonce', 'Dua Lipa']; // object storing track artists
+songTitles = ["Don't Hurt Yourself", "Don't Start Now"]; // object storing track titles
+
+// function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
 let playing = true;
-let songIndex = 0;
-
-const songs = ['./assets/music/beyonce.mp3', './assets/music/dontstartnow.mp3'];
-const thumbnails = ['./assets/images/lemonade.png', './assets/images/dontstartnow.png'];
-const songArtists = ['Beyonce', 'Dua Lipa'];
-const songTitles = ["Don't Hurt Yourself", "Don't Start Now"];
-
-// Play / Pause
 function playPause() {
     if (playing) {
-        pPause.src = "./assets/icons/pause.png";
+        const song = document.querySelector('#song'),
+        thumbnail = document.querySelector('#thumbnail');
+
+        pPause.src = "./assets/icons/pause.png"
         thumbnail.style.transform = "scale(1.15)";
+        
         song.play();
         playing = false;
     } else {
-        pPause.src = "./assets/icons/play.png";
-        thumbnail.style.transform = "scale(1)";
+        pPause.src = "./assets/icons/play.png"
+        thumbnail.style.transform = "scale(1)"
+        
         song.pause();
         playing = true;
     }
 }
 
-// Next Song
+// automatically play the next song at the end of the audio object's duration
+song.addEventListener('ended', function(){
+    song.pause()
+    // nextSong();
+});
+
+// function where songIndex is incremented, song/thumbnail image/background image/song artist/song title changes to next index value, and playPause() runs to play next track 
 function nextSong() {
     songIndex++;
-    if (songIndex >= songs.length) songIndex = 0;
-
+    if (songIndex > 1) {
+        songIndex = 0;
+    };
     song.src = songs[songIndex];
     thumbnail.src = thumbnails[songIndex];
     background.src = thumbnails[songIndex];
+
     songArtist.innerHTML = songArtists[songIndex];
     songTitle.innerHTML = songTitles[songIndex];
 
@@ -45,14 +57,16 @@ function nextSong() {
     playPause();
 }
 
-// Previous Song
+// function where songIndex is decremented, song/thumbnail image/background image/song artist/song title changes to previous index value, and playPause() runs to play previous track 
 function previousSong() {
     songIndex--;
-    if (songIndex < 0) songIndex = songs.length - 1;
-
+    if (songIndex < 0) {
+        songIndex = 1;
+    };
     song.src = songs[songIndex];
     thumbnail.src = thumbnails[songIndex];
     background.src = thumbnails[songIndex];
+
     songArtist.innerHTML = songArtists[songIndex];
     songTitle.innerHTML = songTitles[songIndex];
 
@@ -60,33 +74,32 @@ function previousSong() {
     playPause();
 }
 
-// Update progress bar + times
+// update progressBar.max to song object's duration, same for progressBar.value, update currentTime/duration DOM
 function updateProgressValue() {
-    progressBar.max = song.duration || 0;
-    progressBar.value = song.currentTime || 0;
+    progressBar.max = song.duration;
+    progressBar.value = song.currentTime;
+    document.querySelector('.currentTime').innerHTML = (formatTime(Math.floor(song.currentTime)));
+    if (document.querySelector('.durationTime').innerHTML === "NaN:NaN") {
+        document.querySelector('.durationTime').innerHTML = "0:00";
+    } else {
+        document.querySelector('.durationTime').innerHTML = (formatTime(Math.floor(song.duration)));
+    }
+};
 
-    document.querySelector('.currentTime').innerHTML = formatTime(Math.floor(song.currentTime));
-    document.querySelector('.durationTime').innerHTML =
-        isNaN(song.duration) ? "0:00" : formatTime(Math.floor(song.duration));
-}
-
-// Format MM:SS
+// convert song.currentTime and song.duration into MM:SS format
 function formatTime(seconds) {
-    let min = Math.floor(seconds / 60);
-    let sec = Math.floor(seconds % 60);
-    if (sec < 10) sec = `0${sec}`;
+    let min = Math.floor((seconds / 60));
+    let sec = Math.floor(seconds - (min * 60));
+    if (sec < 10){ 
+        sec  = `0${sec}`;
+    };
     return `${min}:${sec}`;
-}
+};
 
-// Change progress manually
+// run updateProgressValue function every 1/2 second to show change in progressBar and song.currentTime on the DOM
+setInterval(updateProgressValue, 500);
+
+// function where progressBar.value is changed when slider thumb is dragged without auto-playing audio
 function changeProgressBar() {
     song.currentTime = progressBar.value;
-}
-
-// Setup Event Listeners
-document.addEventListener("DOMContentLoaded", () => {
-    pPause.addEventListener("click", playPause);
-    progressBar.addEventListener("change", changeProgressBar);
-    song.addEventListener("ended", nextSong);
-    setInterval(updateProgressValue, 500);
-});
+};
